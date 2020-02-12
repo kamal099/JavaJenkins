3583170b25d09c2567e7d7386c23d85604ab4ff6
kamal099 <kamal_afg786@hotmail.com>
2020-02-11 22:00:23 -0500

    FirstCommittoJava

---
+++ src/FirsrFile.java
@@ -0,0 +1,12 @@
+
+public class FirsrFile {
+
+	public static void main(String[] args) {
+
+		for(int a=1;a<=10;a++) {
+			
+			System.out.println("First Jenkins Project from java file"+ a);
+		}
+	}
+
+}
