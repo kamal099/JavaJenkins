1621fc81656af391237fa22a0e39e59fb70f6d40
kamal099 <57638031+kamal099@users.noreply.github.com>
2020-02-13 11:08:13 -0500

    new java commit

---
+++ src/dowhile.java
@@ -0,0 +1,8 @@
+
+public class dowhile {
+
+	public static void main(String[] args) {
+
+System.out.println("Im standalone message");
+}
+}
---
+++ src/whileLoop.java
@@ -0,0 +1,13 @@
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+
+		int i=0;
+		while(i<10) {
+			System.out.println("While loop printing" + i);
+			i++;
+		}
+	}
+
+}
